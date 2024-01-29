95076993ea6970f19c114a0c1b2166eaca4cb7d1
JinYuShao <a1009943858@gmail.com>
2024-01-29 14:18:30 +0800

    fix: react-native-web color  inherited

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@
  */
 import './global.css';
 import {AppProvider} from '@legoo/headless';
-import {Platform, Text, View} from 'react-native';
+import {Platform, Text} from 'react-native';
 import React from 'react';
 import RootStack from './src/navigation/rootStack';
 
@@ -18,12 +18,9 @@ function App(): React.JSX.Element {
   );
   if (Platform.OS === 'web') {
     // Why wrapped by Text component?
+    // Create a TextAncestorContext, make the color of the child Text component inherited
     // Related issues: https://github.com/necolas/react-native-web/issues/2634
-    return (
-      <Text className="text-foreground">
-        <View className="w-screen h-screen">{content}</View>
-      </Text>
-    );
+    return <Text className="flex flex-1 text-foreground">{content}</Text>;
   }
   return content;
 }
